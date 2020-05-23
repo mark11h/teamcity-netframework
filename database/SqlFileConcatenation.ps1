2c6434333734a9142634f89d44b8c952d7481bfc
Mark Hinchliffe <mark11h@live.co.uk>
2020-05-23 17:17:55 +0100

    minor change

--- database/SqlFileConcatenation.ps1
+++ database/SqlFileConcatenation.ps1
@@ -8,19 +8,19 @@ Get-Content -Path $database\*.sql | Out-File -FilePath $outfile -encoding ASCII
 
 Add-Content $outfile "`n"
 
-$users = -join($location,"\Setup")
+$users = -join($location,"\setup")
 
 Get-Content -Path $users\*.sql | Out-File -FilePath $outfile -Append -encoding ASCII
 
 Add-Content $outfile "`n"
 
-$patches = -join($location,"\Patches")
+$patches = -join($location,"\patches")
 
 Get-Content -Path $patches\*.sql | Out-File -FilePath $outfile -Append -encoding ASCII
 
 Add-Content $outfile "`n"
 
-$data = -join($location,"\Data")
+$data = -join($location,"\data")
 
 Get-Content -Path $data\*.sql | Out-File -FilePath $outfile -Append -encoding ASCII
 
