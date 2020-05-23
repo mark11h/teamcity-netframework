455ab792a988cfaacd748b9b33cdd2268de7f1ac
Mark Hinchliffe <mark11h@live.co.uk>
2020-05-23 17:05:21 +0100

    added database and unit test examples

---
+++ unit-tests/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Web.Mvc;
+using teamcity_netframework.Controllers;
+
+namespace unit_tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Index_loads_expected_view()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
